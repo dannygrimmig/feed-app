84dc0ba8a8b3d7a5fd96af48f680da6bb166cfb0
DannyGrimmig <dannygrimmig17@gmail.com>
2024-02-28 22:26:42 -0500

    move to env file

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDXhKj9UfDpMLFEqg6QcY5zCnMGfZhv_1c",
-  authDomain: "feed-development.firebaseapp.com",
-  projectId: "feed-development",
-  storageBucket: "feed-development.appspot.com",
-  messagingSenderId: "1013118693817",
-  appId: "1:1013118693817:web:de6f37dad2c1a3c6770668",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
