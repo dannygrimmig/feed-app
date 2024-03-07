947fd119c5e9ca30f43276ab6ea2e24b1543acb5
DannyGrimmig <dannygrimmig17@gmail.com>
2024-03-07 00:36:42 -0500

    write recipes to database, format next

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -5,75 +5,186 @@ import { useAuth } from "../../contexts/AuthContext";
 import { addNewRecipeToDataBase } from "../../api/recipes";
 
 export function Profile() {
-  //imported
+  // imported
   const { currentUser, currentUserEmail } = useAuth();
 
-  //derived
-  const displayedRecipes = getRecipesByUserId(currentUser.email);
+  // managed
+  const [name, setName] = React.useState("");
+  const [image, setImage] = React.useState("");
+  const [serves, setServes] = React.useState(0);
+  const [time, setTime] = React.useState(0);
+
+  const [newIngredient, setNewIngredient] = React.useState("");
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const [newDirection, setNewDirection] = React.useState("");
+  const [directions, setDirections] = React.useState([]);
+
+  // derived
+  const [displayedRecipes, setDisplayedRecipes] = React.useState(
+    getRecipesByUserId(currentUser.email)
+  );
 
   // refs
-  const titleRef = React.useRef();
-  const imageRef = React.useRef();
-  const servesRef = React.useRef();
-  const timeRef = React.useRef();
+  const ingredientRef = React.useRef();
+  const directionsRef = React.useRef();
 
   function handleSubmit(e) {
     e.preventDefault();
     const newRecipe = {
-      name: titleRef.current.value,
-      image: imageRef.current.value,
-      stars: 0,
-      serves: servesRef.current.value,
-      time: timeRef.current.value,
-      ingredients: ["i0", "i1"],
-      directions: ["d0", "d1"],
+      chef: currentUserEmail,
+      name: name,
+      image: image,
+      serves: serves,
+      time: time,
+      ingredients: ingredients,
+      directions: directions,
     };
     addNewRecipeToDataBase(newRecipe, currentUser);
+    setDisplayedRecipes([...displayedRecipes, newRecipe]);
+    clearForm();
+  }
+
+  function clearForm() {
+    setName("");
+    setImage("");
+    setServes(0);
+    setTime(0);
+    setIngredients([]);
+    setDirections([]);
+  }
+
+  function handleIngredientSubmit(e) {
+    // defense
+    if (newIngredient.length <= 0) {
+      return;
+    }
+
+    setIngredients([...ingredients, newIngredient]);
+    setNewIngredient("");
+    ingredientRef.current.focus();
+  }
+
+  function handleDirectionSubmit(e) {
+    // defense
+    if (newDirection.length <= 0) {
+      return;
+    }
+
+    setDirections([...directions, newDirection]);
+    setNewDirection("");
+    directionsRef.current.focus();
   }
 
   return (
     <div>
       <h1>current uid: {currentUser}</h1>
-      <h1>current email: {currentUserEmail}</h1>
+      <h1 className="mb-4">current email: {currentUserEmail}</h1>
 
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>title</label>
-          <input
-            ref={titleRef}
-            type="text"
-            className="border rounded px-2 py-1 w-full"
-          />
+        <div className="grid grid-cols-2">
+          <div className="flex flex-col gap-6">
+            <input
+              value={name}
+              onChange={({ target }) => setName(target.value)}
+              type="text"
+              placeholder="title"
+              className="outline-none text-3xl font-header"
+            />
+
+            <div className="flex items-center">
+              <div className="flex gap-2">
+                <label>serves: </label>
+                <input
+                  value={serves}
+                  onChange={({ target }) => setServes(target.value)}
+                  type="number"
+                  className="outline-none w-8"
+                />
+              </div>
+
+              <div className="flex gap-2">
+                <label>time: </label>
+                <input
+                  value={time}
+                  onChange={({ target }) => setTime(target.value)}
+                  type="number"
+                  className="outline-none w-8"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label>ingredients</label>
+
+              <ol className="pl-4 list-disc">
+                {ingredients.map((ingredient, index) => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+              </ol>
+
+              <div className="flex items-center gap-2">
+                <input
+                  ref={ingredientRef}
+                  type="text"
+                  value={newIngredient}
+                  onChange={({ target }) => setNewIngredient(target.value)}
+                  className="border rounded px-2 py-1 w-full"
+                />
+
+                <button
+                  type="button"
+                  onClick={handleIngredientSubmit}
+                  className="border rounded-full px-4 py-2"
+                >
+                  add
+                </button>
+              </div>
+            </div>
+
+            <div>
+              <label>directions</label>
+
+              <ol className="pl-4 list-decimal">
+                {directions.map((direction, index) => (
+                  <li key={direction}>{direction}</li>
+                ))}
+              </ol>
+
+              <div className="flex items-center gap-2">
+                <input
+                  ref={directionsRef}
+                  type="text"
+                  value={newDirection}
+                  onChange={({ target }) => setNewDirection(target.value)}
+                  className="border rounded px-2 py-1 w-full"
+                />
+
+                <button
+                  type="button"
+                  onClick={handleDirectionSubmit}
+                  className="border rounded-full px-4 py-2"
+                >
+                  add
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-8 flex flex-col justify-center">
+            <img src={image} alt="" />
+
+            <input
+              value={image}
+              onChange={({ target }) => setImage(target.value)}
+              type="text"
+              placeholder="image url"
+              className="border rounded px-2 py-1 w-full"
+            />
+          </div>
         </div>
 
-        <div>
-          <label>image</label>
-          <input
-            ref={imageRef}
-            type="text"
-            className="border rounded px-2 py-1 w-full"
-          />
-        </div>
-
-        <div>
-          <label>serves</label>
-          <input
-            ref={servesRef}
-            type="number"
-            className="border rounded px-2 py-1 w-full"
-          />
-        </div>
-
-        <div>
-          <label>time</label>
-          <input
-            ref={timeRef}
-            type="number"
-            className="border rounded px-2 py-1 w-full"
-          />
-        </div>
-
-        <button type="submit" className="border rounded-full px-4 py-2">
+        <button type="submit" className="border rounded-full px-4 py-2 w-max">
           add recipe
         </button>
       </form>
